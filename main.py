586492cf2724de5f11da03c871656f395ef1aa7b
jshlife <s230278@baeksin.ms.kr>
2024-07-24 12:18:56 +0900

    main.py

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ if uploaded_file is not None:
     # 중학생 연령대 추출
     middle_school_ages = ['2024년06월_계_12세', '2024년06월_계_13세', '2024년06월_계_14세']
 
+    # 고령 연령대 추출
+    elderly_ages = ['2024년06월_계_65세 이상']
+
     # 지역 선택
     selected_region = st.selectbox('지역을 선택하세요:', data['행정구역'].unique())
 
@@ -22,17 +25,22 @@ if uploaded_file is not None:
     # 중학생 연령대 인구수 합계
     middle_school_population = region_data[middle_school_ages].apply(lambda x: x.str.replace(',', '').astype(int)).sum(axis=1).values[0]
 
+    # 고령 연령대 인구수 합계
+    elderly_population = region_data[elderly_ages].apply(lambda x: x.str.replace(',', '').astype(int)).sum(axis=1).values[0]
+
     # 총 인구수
     total_population = int(region_data['2024년06월_계_총인구수'].str.replace(',', '').values[0])
 
     # 비율 계산
     middle_school_ratio = (middle_school_population / total_population) * 100
+    elderly_ratio = (elderly_population / total_population) * 100
+    other_ratio = 100 - middle_school_ratio - elderly_ratio
 
     # 원 그래프 생성
-    labels = ['중학생 연령대', '기타 연령대']
-    sizes = [middle_school_ratio, 100 - middle_school_ratio]
-    colors = ['#ff9999', '#66b3ff']
-    explode = (0.1, 0)
+    labels = ['중학생 연령대', '고령 연령대', '기타 연령대']
+    sizes = [middle_school_ratio, elderly_ratio, other_ratio]
+    colors = ['#ff9999', '#66b3ff', '#99ff99']
+    explode = (0.1, 0.1, 0)
 
     fig1, ax1 = plt.subplots()
     ax1.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%',
